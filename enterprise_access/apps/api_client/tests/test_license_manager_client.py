250117a3a619546b8abfd9ac3126755ed1631626
Alexander Dusenbery <adusenbery@2u.com>
2025-04-14 14:11:09 -0400

    feat: client methods to create customer agreement and subs plan

--- enterprise_access/apps/api_client/tests/test_license_manager_client.py
+++ enterprise_access/apps/api_client/tests/test_license_manager_client.py
@@ -44,6 +44,36 @@ class TestLicenseManagerApiClient(TestCase):
             timeout=settings.LICENSE_MANAGER_CLIENT_TIMEOUT,
         )
 
+    @mock.patch('enterprise_access.apps.api_client.base_oauth.OAuthAPIClient')
+    def test_get_customer_agreement(self, mock_oauth_client):
+        mock_get = mock.Mock()
+        mock_get.return_value = Response()
+        mock_oauth_client.get = mock_get
+
+        lm_client = LicenseManagerApiClient()
+        lm_client.get_customer_agreement('some-customer-uuid')
+
+        expected_url = (
+            'http://license-manager.example.com'
+            '/api/v1/customer-agreement/?enterprise_customer_uuid=some-customer-uuid'
+        )
+        mock_oauth_client.return_value.get.assert_called_with(expected_url)
+
+    @mock.patch('enterprise_access.apps.api_client.base_oauth.OAuthAPIClient')
+    def test_create_customer_agreement(self, mock_oauth_client):
+        mock_get = mock.Mock()
+        mock_get.return_value = Response()
+        mock_oauth_client.get = mock_get
+
+        lm_client = LicenseManagerApiClient()
+        lm_client.get_customer_agreement('some-customer-uuid')
+
+        expected_url = (
+            'http://license-manager.example.com'
+            '/api/v1/customer-agreement/?enterprise_customer_uuid=some-customer-uuid'
+        )
+        mock_oauth_client.return_value.get.assert_called_with(expected_url)
+
 
 class TestLicenseManagerUserApiClient(MockLicenseManagerMetadataMixin):
     """
