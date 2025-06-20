efb2a4c9dd9b63757df6d42cc66960c77e8364a7
omar-sarfraz <omar.sarfraz@arbisoft.com>
2025-06-20 13:01:56 +0500

    feat: added cancel api endpoint for learner credit request

--- enterprise_access/apps/api/serializers/__init__.py
+++ enterprise_access/apps/api/serializers/__init__.py
@@ -45,6 +45,7 @@ from .subsidy_access_policy import (
 )
 from .subsidy_requests import (
     CouponCodeRequestSerializer,
+    LearnerCreditRequestCancelSerializer,
     LearnerCreditRequestDeclineSerializer,
     LearnerCreditRequestSerializer,
     LicenseRequestSerializer,
