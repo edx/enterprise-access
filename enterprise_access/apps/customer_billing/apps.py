96616e712e729f7579717afa1ca2e740a4c8795d
gshivajibiradar <gshivajibiradar-sonata@2u.com>
2026-05-21 09:05:28 +0000

    chore: guard customer_billing signal wiring in app config

--- enterprise_access/apps/customer_billing/apps.py
+++ enterprise_access/apps/customer_billing/apps.py
@@ -7,6 +7,11 @@ class CustomerBillingConfig(AppConfig):
     """ App config for customer_billing. """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'enterprise_access.apps.customer_billing'
+    _signals_wired = False
 
     def ready(self):
+        # Prevent duplicate receiver registration when Django reloads app configs.
+        if self._signals_wired:
+            return
         import enterprise_access.apps.customer_billing.signals  # pylint: disable=import-outside-toplevel,unused-import
+        self._signals_wired = True
