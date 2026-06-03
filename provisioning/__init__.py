dba35f7b5c31d98ce4ed71e6b9954a6af9380e57
gshivajibiradar <gshivajibiradar-sonata@2u.com>
2026-06-03 16:25:15 +0000

    chore: remove provisioning compatibility shim files from PR

--- provisioning/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Compatibility package for tooling that imports provisioning.* modules."""
--- provisioning/models.py
+++
@@ -1,3 +0,0 @@
-"""Compatibility shim for pylint-django foreign key model resolution."""
-
-from enterprise_access.apps.provisioning.models import *  # noqa: F401,F403
